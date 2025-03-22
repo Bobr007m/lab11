bd66f7a4cd8684888dc174462c1bd12c1c39f937
Bobr007m <masha_kovaleva_06@mail.ru>
2025-03-22 12:03:45 +0500

    1 part

--- Program.cs
+++ Program.cs
@@ -1,15 +1,97 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Geometryclass;
 
-namespace lab11
+namespace NonGenericCollections
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Создание стека
+            Stack stack = new Stack();
+
+            // Добавление объектов
+            stack.Push(new Circle1(5));
+            stack.Push(new Parallelepiped1(10));
+            stack.Push(new Rectangle1(4, 6));
+
+            Console.WriteLine("Элементы в стеке:");
+            foreach (var item in stack)
+            {
+                if (item is IShowable showable)
+                    showable.Show();
+            }
+
+            // Удаление элемента
+            Console.WriteLine("\nУдаляем верхний элемент:");
+            var removed = stack.Pop();
+            Console.WriteLine($"Удален: {removed}");
+
+            // Поиск элемента
+            Console.WriteLine("\nПоиск элемента:");
+            bool contains = stack.Contains(new Circle1(5));
+            Console.WriteLine($"Содержит круг с радиусом 5: {contains}");
+
+            // Клонирование коллекции
+            Console.WriteLine("\nКлонирование стека:");
+            Stack clonedStack = (Stack)stack.Clone();
+            Console.WriteLine("Клонированный стек:");
+            foreach (var item in clonedStack)
+            {
+                if (item is IShowable showableClone)
+                    showableClone.Show();
+            }
+
+            // Сортировка (не поддерживается в Stack, поэтому преобразуем в List)
+            Console.WriteLine("\nСортировка элементов:");
+            ArrayList sortedList = new ArrayList(stack);
+            sortedList.Sort();
+            foreach (var item in sortedList)
+            {
+                if (item is IShowable showableSorted)
+                    showableSorted.Show();
+            }
+            // 2 задание
+                // Создание словаря
+                SortedDictionary<string, IGeometricFigure1> dictionary = new SortedDictionary<string, IGeometricFigure>();
+
+                // Добавление объектов
+                dictionary.Add("Circle1", new Circle1(5));
+                dictionary.Add("Parallelepiped1", new Parallelepiped1(10));
+                dictionary.Add("Rectangle1", new Rectangle1(4, 6));
+
+                Console.WriteLine("Элементы в словаре:");
+                foreach (var kvp in dictionary)
+                {
+                    Console.WriteLine($"{kvp.Key}:");
+                    kvp.Value.Show();
+                }
+
+                // Удаление элемента
+                Console.WriteLine("\nУдаляем элемент 'Circle1':");
+                dictionary.Remove("Circle1");
+
+                // Поиск элемента
+                Console.WriteLine("\nПоиск элемента:");
+                bool containsKey = dictionary.ContainsKey("Parallelepiped1");
+                Console.WriteLine($"Содержит ключ 'Parallelepiped1': {containsKey}");
+
+                // Клонирование коллекции
+                Console.WriteLine("\nКлонирование словаря:");
+                SortedDictionary<string, IGeometricFigure> clonedDictionary = new SortedDictionary<string, IGeometricFigure>(dictionary);
+                foreach (var kvp in clonedDictionary)
+                {
+                    Console.WriteLine($"{kvp.Key}:");
+                    kvp.Value.Show();
+                }
+
+                // Сортировка (автоматическая в SortedDictionary)
+                Console.WriteLine("\nСловарь уже отсортирован по ключам.");
+           // 3 задание
+            TestCollections test = new TestCollections(1000);
+            test.MeasurePerformance();
+        }
         }
     }
-}
---
+++ TestCollections.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PerformanceTesting
+{
+    public class TestCollections
+    {
+        public SortedSet<IGeometricFigure> Collection1 { get; set; }
+        public SortedSet<string> Collection2 { get; set; }
+        public HashSet<IGeometricFigure> Collection3 { get; set; }
+        public HashSet<string> Collection4 { get; set; }
+
+        public TestCollections(int size)
+        {
+            Collection1 = new SortedSet<IGeometricFigure>();
+            Collection2 = new SortedSet<string>();
+            Collection3 = new HashSet<IGeometricFigure>();
+            Collection4 = new HashSet<string>();
+
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                var figure = new Circle(rand.Next(1, 100));
+                Collection1.Add(figure);
+                Collection2.Add(figure.ToString());
+                Collection3.Add(figure);
+                Collection4.Add(figure.ToString());
+            }
+        }
+
+        public void MeasurePerformance()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            // Первый элемент
+            var firstElement = Collection1.Min;
+            MeasureTime(Collection1, firstElement, "SortedSet<T>");
+            MeasureTime(Collection2, firstElement.ToString(), "SortedSet<string>");
+            MeasureTime(Collection3, firstElement, "HashSet<T>");
+            MeasureTime(Collection4, firstElement.ToString(), "HashSet<string>");
+
+            // Центральный элемент
+            var middleElement = GetMiddleElement(Collection1);
+            MeasureTime(Collection1, middleElement, "SortedSet<T>");
+            MeasureTime(Collection2, middleElement.ToString(), "SortedSet<string>");
+            MeasureTime(Collection3, middleElement, "HashSet<T>");
+            MeasureTime(Collection4, middleElement.ToString(), "HashSet<string>");
+
+            // Последний элемент
+            var lastElement = Collection1.Max;
+            MeasureTime(Collection1, lastElement, "SortedSet<T>");
+            MeasureTime(Collection2, lastElement.ToString(), "SortedSet<string>");
+            MeasureTime(Collection3, lastElement, "HashSet<T>");
+            MeasureTime(Collection4, lastElement.ToString(), "HashSet<string>");
+
+            // Элемент, которого нет
+            var nonExistent = new Circle(1000);
+            MeasureTime(Collection1, nonExistent, "SortedSet<T>");
+            MeasureTime(Collection2, nonExistent.ToString(), "SortedSet<string>");
+            MeasureTime(Collection3, nonExistent, "HashSet<T>");
+            MeasureTime(Collection4, nonExistent.ToString(), "HashSet<string>");
+        }
+
+        private void MeasureTime<T>(ICollection<T> collection, T element, string collectionName)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            bool contains = collection.Contains(element);
+            sw.Stop();
+            Console.WriteLine($"{collectionName}: поиск элемента {(contains ? "найден" : "не найден")} за {sw.ElapsedTicks} тиков.");
+        }
+
+        private IGeometricFigure GetMiddleElement(SortedSet<IGeometricFigure> set)
+        {
+            int index = set.Count / 2;
+            int i = 0;
+            foreach (var item in set)
+            {
+                if (i == index) return item;
+                i++;
+            }
+            return null;
+        }
+    }
+}
